06ef94f814d2a170d14c0a3b52fb37c2a863c26b
Sebastian Lara <sebastianlaramachado@gmail.com>
2021-11-19 17:32:38 -0500

    Updated Rewarder contract

--- contracts/TodoRewarder.sol
+++ contracts/TodoRewarder.sol
@@ -4,25 +4,40 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract TodoRewarder {
-    uint public taskCount = 0;
-
+    // Where we record information.
     struct Task {
         uint id;
+        string name;
         string content;
         bool completed;
     }
 
-    constructor(){
-        createTask("This is a trial task! Check me out so you get a reward!");
+    constructor(){}
+
+    //Every address has an array of listIds()
+    mapping(address => uint[]) public userTaskIds;
+
+    //Mapping of address to task
+    mapping( address => Task[]) public userTaskList;
+
+    //Setting the task to the blockchain
+    function insertTask (string memory _content, string memory _name) public returns (bool success){
+        if(userTaskIds[msg.sender].length == 0){
+            userTaskList[msg.sender].push(Task(0,_name,_content,false));
+            userTaskIds[msg.sender].push(0);
+            return true;
+        } 
+        // Checking the last latest value of my array containing the id's of the task.
+        uint lastItem = userTaskIds[msg.sender][userTaskIds[msg.sender].length - 1];
+        // Setting the values on the blockchain
+        userTaskList[msg.sender].push(Task(lastItem+1,_name,_content,false));
+        //Upadting userTaskId array of the user
+        userTaskIds[msg.sender].push(lastItem+1);
+        return true;
     }
 
-    mapping(uint => Task) public tasks;
-
-    function createTask(string memory _content) public {
-        taskCount ++;
-        tasks[taskCount] = Task(taskCount, _content, true);
+    function getTaskList (address _userAddress) public view returns(Task[] memory _taskList) {
+        return userTaskList[_userAddress];
     }
-
-    function getTask() public {}
 
 }
--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { ethers } from "ethers";
+import "./App.css";
+import TodoRewarder from "./artifacts/contracts/TodoRewarder.sol/TodoRewarder.json";
+
+const todoRewarderAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+async function requestAccount() {}
+
+async function fetchTodos() {
+  if (typeof window.ethereum !== "undefined") {
+    // Provider allows a connection to the ethereum network and provides a read only access to the blockchain and its status.
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // abstraction that allows conection to specific contract
+    const contract = new ethers.contract(
+      todoRewarderAddress,
+      TodoRewarder.abi,
+      provider
+    );
+    try {
+      // const data = await contract.
+    } catch (err) {
+      console.log(Err, "<- Error");
+    }
+  }
+}
+
+async function setTodo() {}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
