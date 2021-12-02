265d6f9620763a68b7bd995121cf57ac826a53e5
Sebastian Lara <sebastianlaramachado@gmail.com>
2021-12-02 15:38:49 -0500

    Updated TaskListDisplay component

--- src/Components/LandingForm/TaskListDisplay/TaskListDisplay.jsx
+++ src/Components/LandingForm/TaskListDisplay/TaskListDisplay.jsx
@@ -1,20 +1,14 @@
 import React from "react";
+import { Row, Container } from "react-bootstrap";
+import "./TaskListDisplay.css";
 
-const TaskListDisplay = (fetchedTask) => {
+const TaskListDisplay = ({ fetchedTask }) => {
   return (
-    <div>
-      <h5>
-        {" "}
-        {fetchedTask &&
-          fetchedTask.fetchedTask &&
-          fetchedTask.fetchedTask.name}{" "}
-      </h5>
-      <h5>
-        {" "}
-        {fetchedTask &&
-          fetchedTask.fetchedTask &&
-          fetchedTask.fetchedTask.content}{" "}
-      </h5>
+    <div id="task__list__container">
+      <div>Hi</div>
+      {/* {console.log(fetchedTask, "This is my Fetched Task")}
+      <h5> {fetchedTask && fetchedTask.name} </h5>
+      <h5> {fetchedTask && fetchedTask.content} </h5> */}
     </div>
   );
 };
